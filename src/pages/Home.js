f40327528c6c8709ad74ef2d0d324678b43f6ac4
amaralazizy <21-101152@students.eui.edu.eg>
2024-05-04 03:03:37 +0300

    Amarico Introduction

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
 export default function Home(){
-    return <div className="">
-        hello
-    </div>
+    return (
+      <div className="text-6xl flex justify-center items-center h-screen">
+        I am Amarico, First time to use react fe tar7eeb wla 1000 w arga3 tany
+      </div>
+    );
 }
