e6509e6e464146eacee51e336ee7eb52e9eb5592
Abdullah Ayman <abdullahaymanmsre@gmail.com>
2024-05-13 17:28:41 +0300

    api integration setup

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,7 @@
 export const UserTypes = {
-    STUDENT: 'student',
-    TEACHER: 'teacher'
-}
+  STUDENT: "student",
+  TEACHER: "teacher",
+};
+
+export const TOKEN_STORAGE = "token";
+export const HOST = "";
