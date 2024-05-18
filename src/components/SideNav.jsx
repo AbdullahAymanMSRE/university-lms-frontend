77daf1ce6d0164235b3b6fde5c7ca6c7408816c6
amaralazizy <21-101152@students.eui.edu.eg>
2024-05-19 02:04:20 +0300

    Responsiveness Update

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -11,30 +11,26 @@ export default function SideNav({ items, userType, className }) {
   return (
     <nav
       className={twMerge(
-        "bg-white p-10 flex flex-col gap-16",
+        `bg-white p-10 flex flex-col gap-16 transition z-[999] min-h-screen ease-in-out max-md:fixed max-md:left-0 max-md:top-0 max-md:w-full`,
         className
       )}
     >
       <div className="logo h-11 flex items-center gap-3">
         <div className={`h-full w-11 rounded-xl ${bgClr}`}></div>
-        <div
-          className={`${textClr} text-2xl leading-5 font-bold`}
-        >
+        <div className={`${textClr} text-2xl leading-5 font-bold`}>
           {isStudent ? "Student" : "Instructor"}
           <br />
-          <span className="text-xs font-medium uppercase">
-            Dashboard
-          </span>
+          <span className="text-xs font-medium uppercase">Dashboard</span>
         </div>
       </div>
-      <ul className=" w-52 text-base flex flex-col gap-4">
+      <ul className="w-52 text-base flex flex-col gap-4">
         {items.map((item, index) => (
           <li key={index} className={twMerge("")}>
             <NavLink end to={item.url}>
               {({ isActive, isPending, isTransitioning }) => (
                 <div
                   className={twMerge(
-                    "pl-3 gap-3 rounded-md h-11 flex items-center ",
+                    "pl-3 gap-3 rounded-md h-11 flex items-center",
                     clsx({
                       [bgClr]: isActive,
                     })
--- src/pages/student/Home.jsx
+++ src/pages/student/Home.jsx
@@ -8,8 +8,9 @@ export default function Home() {
   const student = {
     name: "John Doe",
   };
+  const [toggle, setToggle] = React.useState(false);
   return (
-    <body className="h-screen bg-pageColor flex">
+    <body className="bg-pageColor relative flex max-md:flex-col">
       <SideNav
         items={[
           {
@@ -44,15 +45,45 @@ export default function Home() {
           },
         ]}
         userType={UserTypes.STUDENT}
+        className={`${toggle ? "max-md:translate-x-0" : "max-md:-translate-x-full"}`}
       />
-      <div className="flex w-full p-7 flex-col gap-7">
-        <div className="font-bold">
-          <p className="text-sm text-customBlue mb-3">
-            Hi {student.name},
-          </p>
-          <h1 className="text-4xl text-darkenedBlue">
-            Welcome to EUI!
-          </h1>
+      <div
+        className={`fixed h-14 flex items-center justify-center max-md:z-[1000] ${toggle ? "max-md:right-4" : "max-md:left-4"}`}
+      >
+        <button
+          onClick={() => {
+            setToggle(!toggle);
+          }}
+          className={`flex items-center justify-center
+          `}
+        >
+          <div className="hidden max-md:flex h-10 w-10 cursor-pointer items-center justify-center rounded-3xl bg-transparent p-2">
+            <div className="space-y-2">
+              <span
+                className={`block h-1 w-8 origin-center rounded-full ${
+                  toggle
+                    ? "bg-primary w-8 translate-y-1.5 rotate-45"
+                    : "bg-white"
+                } transition-transform ease-in-out`}
+              ></span>
+              <span
+                className={`block h-1 w-6 origin-center rounded-full ${
+                  toggle
+                    ? "bg-primary w-8 -translate-y-1.5 -rotate-45"
+                    : "bg-white"
+                } transition-transform ease-in-out`}
+              ></span>
+            </div>
+          </div>
+        </button>
+      </div>
+      <div className="hidden z-50 bg-primary text-2xl h-14 w-full text-white uppercase max-md:flex max-md:justify-center max-md:items-center sticky top-0">
+        Dashboard
+      </div>
+      <div className="flex w-full p-7 flex-col gap-7 max-md:items-center max-md:p-4">
+        <div className="font-bold max-md:self-start max-md:hidden">
+          <p className="text-sm text-customBlue mb-3 ">Hi {student.name},</p>
+          <h1 className="text-4xl text-darkenedBlue">Welcome to EUI!</h1>
         </div>
         <Outlet />
       </div>
