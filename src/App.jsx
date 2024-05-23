2614be6fb7856dbfb7b4ccf4c5ec7d11920c907f
Abdullah Ayman <abdullahayman@Abdullahs-Laptop.local>
2024-05-23 15:56:25 +0300

    added rtk setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import MyRouter from "./router/index";
+import store from "./store";
 
 export default function App() {
-	return (
-		<div className="min-h-screen">
-			<MyRouter />
-		</div>
-	);
+  return (
+    <div className="min-h-screen">
+      <Provider store={store}>
+        <MyRouter />
+      </Provider>
+    </div>
+  );
 }
