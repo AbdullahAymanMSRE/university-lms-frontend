637e9e6ee3bae810f248339f87f8b23ca423c451
amaralazizy <21-101152@students.eui.edu.eg>
2024-05-26 22:52:17 +0300

    Expanding Announcement By Click

--- src/pages/student/Announcements.jsx
+++ src/pages/student/Announcements.jsx
@@ -1,3 +1,4 @@
+import { id } from "date-fns/locale";
 import Announcement from "../../components/Announcement";
 
 export default function Announcements() {
@@ -6,14 +7,28 @@ export default function Announcements() {
             <Announcement
                 author="Amar Wael"
                 subject="Quiz 1"
-                content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consectetur libero."
-                courseName={"Math 101"}
+                content="Lorem ipsum dolor sit amet,
+                 consectetur adipiscing elit. Nulla nec purus feugiat,
+                  molestie ipsum et, consectetur libero. Lorem ipsum dolor sit amet,
+                   consectetur adipiscing elit. Nulla nec purus feugiat,
+                    molestie ipsum et, consectetur libero.Lorem ipsum dolor sit amet,
+                     consectetur adipiscing elit. Nulla nec purus feugiat,
+                      molestie ipsum et, consectetur libero.Lorem ipsum dolor sit amet,
+                      consectetur adipiscing elit. Nulla nec purus feugiat,
+                       molestie ipsum et, consectetur libero.Lorem ipsum dolor sit amet,
+                        consectetur adipiscing elit. Nulla nec purus feugiat,
+                         molestie ipsum et, consectetur libero.Lorem ipsum dolor sit amet,
+                          consectetur adipiscing elit. Nulla nec purus feugiat,
+                           molestie ipsum et, consectetur libero.Lorem ipsum dolor sit amet,
+                            consectetur adipiscing elit. Nulla nec purus feugiat,
+                             molestie ipsum et, consectetur libero."
+                course={{ title: "Math 101", id: "1022" }}
             />
             <Announcement
                 author="Amar Wael"
                 subject="Quiz 1"
                 content="Ana number 1"
-                courseName={"Math 102"}
+                course={{ title: "Math 102", id: "1022" }}
             />
         </section>
     );
