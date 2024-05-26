6d4f46cab9385ee4ec1ece385856deed0b0cc71a
Abdelrhman Abdelmoaty <Abdelr7manAbdelmoaty@gmail.com>
2024-05-26 23:11:18 +0300

    updates

--- src/components/AuthHeader.jsx
+++ src/components/AuthHeader.jsx
@@ -1,52 +1,33 @@
 import { UserTypes } from "../lib/constants";
 import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import { useLoginMutation } from "../api/apiSlice";
 import useUserStore from "../store/userStore";
 
 export default function AuthHeader() {
-    const [login, { data, isSuccess, isError }] = useLoginMutation();
     const setUser = useUserStore((state) => state.setUser);
     const location = useLocation();
-
     const navigate = useNavigate();
 
     useEffect(() => {
-        const email = localStorage.getItem("email");
-        const password = localStorage.getItem("password");
-        if (!email || !password) {
+        const _user = JSON.parse(localStorage.getItem("user"));
+        if (!_user) {
             navigate("/login");
             return;
         } else {
-            login({
-                email: email,
-                password: password,
-            });
-        }
-    }, []);
-
-    useEffect(() => {
-        if (data) {
-            setUser(data);
+            setUser(_user);
             if (
                 location.pathname.includes(UserTypes.STUDENT) &&
-                data.role !== UserTypes.STUDENT
+                _user.role !== UserTypes.STUDENT
             ) {
                 navigate("/instructor");
             } else if (
                 location.pathname.includes(UserTypes.INSTRUCTOR) &&
-                data.role !== UserTypes.INSTRUCTOR
+                _user.role !== UserTypes.INSTRUCTOR
             ) {
                 navigate("/student");
             }
         }
-    }, [isSuccess]);
-
-    useEffect(() => {
-        if (isError) {
-            navigate("/login");
-        }
-    }, [isError]);
+    }, []);
 
     return null;
 }
